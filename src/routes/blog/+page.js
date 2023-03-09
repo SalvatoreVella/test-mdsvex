ac394ead455611017e6357812a5c3b1ca50f8d2a
Salvatore Vella <salvatorevella1234@gmail.com>
2023-03-09 14:06:17 +0100

    project not working

---
+++ src/routes/blog/+page.js
@@ -0,0 +1,16 @@
+//const allPosts = import.meta.glob("./**/*.md");
+/*   let body = [];
+  for (let path in allPosts) {
+   body.push(
+    allPosts[path]().then( ({metadata}) => {
+     return { path, metadata}
+    })
+   );
+  }
+  export async function load() {
+    const posts = await Promise.all(body);
+    return {
+      props: { posts },
+    };
+  }
+ */
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let posts;
+  import HelloWorld from "./hello-world.md"
+ </script>
+ 
+ <ul>
+<!--   {#each  posts as {path, metadata: {title}} }
+   <li>
+    <a href={`/blog/${path.replace(".md","")}`}>{title}</a>
+   </li>
+  {/each} -->
+ </ul>
+ <HelloWorld />
